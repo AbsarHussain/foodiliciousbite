41e03ab478ccb3e2bb365cac47e9a8946d41487b
bilal.qayoom <bilal.qayoom@refinitive.com>
2021-06-22 11:39:16 +0500

    'remove'

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   define('DB_SERVER', 'mysql.foodiliciousbite.com');
-   define('DB_USERNAME', 'biteuser');
-   define('DB_PASSWORD', 'Abs159..');
-   define('DB_DATABASE', 'foodiliciousbite');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
-<!-------------->
