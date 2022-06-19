5046b2eae4f62fa663598ce991ef306ec443d74c
Absar Hussain <absarhussain@Absars-MacBook-Pro.local>
2022-06-19 22:32:52 +0500

    carrental

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   define('DB_SERVER', 'sqatest.ceea2phu180k.us-east-1.rds.amazonaws.com');
-   define('DB_USERNAME', 'biteuser');
-   define('DB_PASSWORD', 'Abs159..');
-   define('DB_DATABASE', 'foodiliciousbite');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
-<!-------------->
